009211825743e7a2140c4530428294ecc1112a9a
ariel <ariel@bearrobotics.ai>
2017-07-28 13:07:54 -0700

    first H/W _(3)

--- codeReview_7_27_17.py
+++ codeReview_7_27_17.py
@@ -1,19 +1,11 @@
 #!/usr/bin/python
-<<<<<<< HEAD
 
-x = input ("Enter a number: ")
+x = input("Enter a number: ")
 sum = 0
 i = 1
 
-while i <=x:
-     sum += i
-     i += 1
-print ("sum is: ", sum)
+while x <= i:
+  sum += i
+  i += 1
 
-=======
-x = print ("Enter a number: ") 
-for i in x
-     sum = sum + i
-     i = i + 1
-print ("sum is: ", sum)
->>>>>>> 88d94ec6f11c3bcb28993edd185aedf2202f9ee5
+print "sum is: ", sum
