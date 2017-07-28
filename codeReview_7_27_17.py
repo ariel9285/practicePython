b0a023189aface2f0d7a3946f5d6f2a81297d04c
ariel <ariel@bearrobotics.ai>
2017-07-27 22:05:40 -0700

    1

--- codeReview_7_27_17.py
+++ codeReview_7_27_17.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 
-y = input ("Enter a number: ")
+x = input ("Enter a number: ")
 sum = 0
 i = 1
 
-while i <=y:
+while i <=x:
      sum += i
      i += 1
 print ("sum is: ", sum)
